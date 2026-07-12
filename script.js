5e81189b34ef9d79cc06f1da74f4fb963e078d66
HOSPlTAL <jayden1c3434@gmail.com>
2026-07-12 16:46:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 const links = {
 
-    youtube: "https://www.youtube.com/@%E5%A5%B8",
-    
-    discord: "https://discord.gg/hospital",
-    
-    tiktok: "https://www.tiktok.com/@goodbyeyoucruelworld"
+youtube:"https://www.youtube.com/@%E5%A5%B8",
+discord:"https://discord.gg/hospital",
+tiktok:"https://www.tiktok.com/@goodbyeyoucruelworld"
 
 };
 
 
-
 document.getElementById("youtube").href = links.youtube;
-
 document.getElementById("discord").href = links.discord;
-
 document.getElementById("tiktok").href = links.tiktok;
 
 
@@ -21,53 +16,46 @@ document.getElementById("tiktok").href = links.tiktok;
 
 
 
-const starContainer = document.getElementById("stars");
+
+const stars = document.getElementById("stars");
+
+
+for(let i = 0; i < 300; i++){
+
+
+let star = document.createElement("div");
+
+
+star.className="star";
+
+
+let size = Math.random()*3+1;
+
+
+star.style.width=size+"px";
+star.style.height=size+"px";
+
+
+star.style.left=Math.random()*100+"%";
+star.style.top=Math.random()*100+"%";
 
 
 
-for(let i = 0; i < 220; i++){
-
-
-    const star = document.createElement("div");
-
-
-    star.className = "star";
-
-
-    const size =
-    Math.random() * 3 + 1;
-
-
-    star.style.width =
-    size + "px";
-
-
-    star.style.height =
-    size + "px";
+star.style.opacity=Math.random();
 
 
 
-    star.style.left =
-    Math.random()*100 + "%";
+star.style.animationDuration =
+(Math.random()*40+20)+"s";
 
 
 
-    star.style.top =
-    Math.random()*100 + "%";
+star.style.animationDelay =
+Math.random()*20+"s";
 
 
 
-    star.style.animationDuration =
-    Math.random()*25 + 20 + "s";
-
-
-
-    star.style.animationDelay =
-    Math.random()*20 + "s";
-
-
-
-    starContainer.appendChild(star);
+stars.appendChild(star);
 
 
 }
